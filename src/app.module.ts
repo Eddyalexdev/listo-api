07472924ae41a8ea615d2e81a0a04c5737fb70bb
Eddyalexdev <eddyalex.dev@gmail.com>
2024-02-29 18:50:06 -0400

    refactor: deleted app.service and app.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { CompaniesModule } from './companies/companies.module';
+import { EmployeesModule } from './employees/employees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: `mongodb+srv://eddyalexdev:rRdg3paHcooHfq72@listo.kp6trmi.mongodb.net/?retryWrites=true&w=majority&appName=listo`,
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}']
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true
+    }),
+    CompaniesModule,
+    EmployeesModule
+  ],
 })
 export class AppModule {}
